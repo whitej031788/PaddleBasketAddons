6bbcf52c684edf24b3dec2d71f6bf46230bb559f
Jamie White <whitej031788@gmail.com>
2018-10-18 17:05:44 +0100

    Get Plans and Products API

--- api.php
+++ api.php
@@ -7,15 +7,34 @@ $myReq['vendor_auth_code'] = "d8c55da9085d35e7aead545d3fbfc88cddaf8eb8e2f495340d
 
 $errMsg = "";
 
+if (isset($_GET['method'])) {
+    switch ($_GET['method']) {
+        case "plans":
+            $myURL = "/subscription/plans";
+            $exempt = true;
+            break;
+        case "products":
+            $myURL = "/product/get_products";
+            $exempt = true;
+            break;
+        default:
+            $myURL = "/product/generate_pay_link";
+            $exempt = false;
+            break;
+    }
+} else {
+    $myURL = "/product/generate_pay_link";
+    $exempt = false;
+}
 if (isset($_POST['subscription_id'])) {
     $myReq['product_id'] = $_POST['subscription_id'];
 } else {
-    $errMsg = "No subscription ID passed";
+    $errMsg = $exempt ? "" : "No subscription ID passed";
 }
 if (isset($_POST['recurring_prices'])) {
     $myReq['recurring_prices'] = explode(",", $_POST['recurring_prices']);
 } else {
-    $errMsg = "No recurring price passed";
+    $errMsg = $exempt ? "" : "No recurring price passed";
 }
 if (isset($_POST['title'])) {
     $myReq['title'] = $_POST['title'];
@@ -35,7 +54,10 @@ if (isset($_POST['quantity'])) {
 if (isset($_POST['passthrough'])) {
     $myReq['passthrough'] = $_POST['passthrough'];
 }
-$myReq['expires'] = date('Y-m-d', strtotime("+1 days"));
+
+if (!$exempt) {
+    $myReq['expires'] = date('Y-m-d', strtotime("+1 days"));
+}
 
 try {
     if ($errMsg) {
@@ -43,7 +65,7 @@ try {
     } else {
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL,"https://vendors.paddle.com/api/2.0/product/generate_pay_link");
+        curl_setopt($ch, CURLOPT_URL,"https://vendors.paddle.com/api/2.0" . $myURL);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS,
                     http_build_query($myReq));
