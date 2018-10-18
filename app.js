7720db6c29a263bbef3fd30f04543555c005178f
Sam Pritchard <sampritchard@msn.com>
2018-10-18 16:40:18 +0100

    refactor code

--- app.js
+++ app.js
@@ -9,51 +9,49 @@ $(document).ready(function() {
     $('#subscripton1').click(function() {
         basket.result = basket.result + 35;
         basket.contents.push({name: "Subscription1", id: "123456"});
-        console.log(basket)
-        showBasketAmount();
-        showBasketProducts();
+        showBasketsContent();
     });
 
     $('#subscripton2').click(function() {
         basket.result = basket.result + 70;
         basket.contents.push({name: "Subscription2", id: "123456"});
-        showBasketAmount();
-        showBasketProducts();
+        showBasketsContent();
     });
 
     $('#addition1').click(function() {
         basket.result = basket.result + 2;
         basket.contents.push({name: "Addition1", id: "123456"});
-        showBasketAmount();
-        showBasketProducts();
+        showBasketsContent();
     });
 
     $('#addition2').click(function() {
         basket.result = basket.result + 5;
         basket.contents.push({name: "Addition2", id: "123456"});
-        showBasketAmount();
-        showBasketProducts();
+        showBasketsContent();
     });
 
     $('#addition3').click(function() {
         basket.result = basket.result + 10;
         basket.contents.push({name: "Addition3", id: "123456"});
-        showBasketAmount();
-        showBasketProducts();
+        showBasketsContent();
     });
 
     $('#addition4').click(function() {
         basket.result = basket.result + 15;
         basket.contents.push({name: "Addition4", id: "123456"});
-        showBasketAmount();
-        showBasketProducts();
+        showBasketsContent();
     });
 })
 
 function showBasketAmount() {
     document.getElementById("result").innerHTML = basket.result;
- }
+}
 
  function showBasketProducts() {
-        document.getElementById("contents").innerHTML = JSON.stringify(basket.contents);
- }
+    document.getElementById("contents").innerHTML = JSON.stringify(basket.contents);
+}
+
+function showBasketsContent() {
+    showBasketAmount();
+    showBasketProducts();
+}
